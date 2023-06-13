9905548b28d4402be4e4082154fc9024f65947ef
Brian Williams <heffergod@gmail.com>
2023-06-12 23:13:30 -0700

    updated object, made a json rather than a streamlit

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
 import streamlit as st
+from datetime import datetime
 
+start = st.date_input(
+    "What date are you departing",
+    datetime.date(2023, 6, 12)
+)
+
+end = st.date_input(
+    "What date are you returning",
+    datetime.date(start)
+)
+
+origin = st.text_input('Where are you departing from?', '')
+
+destination = st.text_input('Where are you flying to?', '')
+
+travelers = st.number_input('How many people will be traveling?')
