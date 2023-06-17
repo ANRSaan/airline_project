113c49fa6f45426db3db0ef7e85ce993a516501e
Brian Williams <heffergod@gmail.com>
2023-06-16 19:55:14 -0600

    making a class which will hold the final return

---
+++ Airline_return.py
@@ -0,0 +1,23 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel, StrictStr
+import json
+from Airline_obj import Preferences
+
+class airline_list:
+    def __init__(self) -> None:
+        pass
+
+    def airline_getter(Preferences):
+        ''' 
+            This is where the bit that sends the Preferences object off to the 
+            Selenium scripts goes
+        '''
+
+        '''
+            This is where the bit that waits for the Selenium scripts
+            to finish goes.  It will deposite the returns from those scripts in a
+            
+        '''
+
+        return
